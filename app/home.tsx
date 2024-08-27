db4c5bd4cd9d870b9e6a9790d350843b32068944
Darko Michael Agyeman <agyemanmda@gmail.com>
2024-08-27 11:35:19 +0100

    commit

--- app/home.tsx
+++ app/home.tsx
@@ -1,10 +1,60 @@
+import { useState, useEffect } from "react";
 import { Text, View, StyleSheet } from "react-native";
 import { Colors } from "../constants/Colors";
 import Txt from "../components/Text";
 import { WaveIndicator } from "react-native-indicators";
 import TextButton from "@/components/TextButton";
+import * as Location from "expo-location";
+import io from "socket.io-client";
 
 export default function Screen() {
+  const [location, setLocation] = useState<object>();
+  const [errorMsg, setErrorMsg] = useState<string>("");
+  const socket = io("https://web-socket-setup.onrender.com");
+
+  useEffect(() => {
+    let locationSubscription: Location.LocationSubscription | null;
+
+    const startLocationUpdates = async () => {
+      // Request location permissions
+      let { status } = await Location.requestForegroundPermissionsAsync();
+      if (status !== "granted") {
+        setErrorMsg("Permission to access location was denied");
+        return;
+      }
+
+      // Start watching the location
+      locationSubscription = await Location.watchPositionAsync(
+        {
+          accuracy: Location.Accuracy.High,
+          timeInterval: 5000, // Update every 5 seconds
+          distanceInterval: 10, // Update every 10 meters
+        },
+        (newLocation) => {
+          const { latitude, longitude } = newLocation.coords;
+          setLocation({ latitude, longitude });
+          console.log(location);
+
+          // Send the current location to the WebSocket server
+          socket.emit("locationUpdate", {
+            latitude,
+            longitude,
+            timestamp: new Date().toISOString(),
+          });
+        }
+      );
+    };
+
+    startLocationUpdates();
+
+    return () => {
+      if (locationSubscription) {
+        locationSubscription.remove(); // Stop watching location when the component unmounts
+      }
+      
+    };
+  }, []);
+
   return (
     <View style={styles.body}>
       <WaveIndicator color="#13b80d" size={100} />
--- app/index.tsx
+++ app/index.tsx
@@ -1,67 +1,10 @@
-import { useEffect, useState } from "react";
 import { ImageBackground, StyleSheet, View } from "react-native";
-import * as Location from "expo-location";
 import { FontAwesome5 } from "@expo/vector-icons";
 import Txt from "@/components/Text";
 import IconBtn from "@/components/IconBtn";
 import Heading from "@/components/Heading";
-import io from "socket.io-client";
-
-interface State {
-  lat?: number;
-  lng?: number;
-}
 
 function Welcome() {
-  const [location, setLocation] = useState<State>({
-    lat: undefined,
-    lng: undefined,
-  });
-  const [errorMsg, setErrorMsg] = useState<string>("");
-  const socket = io("https://web-socket-setup.onrender.com");
-
-  useEffect(() => {
-    let locationSubscription: Location.LocationSubscription | null;
-
-    const startLocationUpdates = async () => {
-      // Request location permissions
-      let { status } = await Location.requestForegroundPermissionsAsync();
-      if (status !== "granted") {
-        setErrorMsg("Permission to access location was denied");
-        return;
-      }
-
-      // Start watching the location
-      locationSubscription = await Location.watchPositionAsync(
-        {
-          accuracy: Location.Accuracy.High,
-          timeInterval: 5000, // Update every 5 seconds
-          distanceInterval: 10, // Update every 10 meters
-        },
-        (newLocation) => {
-          const { latitude, longitude } = newLocation.coords;
-          setLocation({ lat: latitude, lng: longitude });
-          console.log(location);
-
-          // Send the current location to the WebSocket server
-          socket.emit("locationUpdate", {
-            latitude,
-            longitude,
-            timestamp: new Date().toISOString(),
-          });
-        }
-      );
-    };
-
-    startLocationUpdates();
-
-    return () => {
-      if (locationSubscription) {
-        locationSubscription.remove(); // Stop watching location when the component unmounts
-      }
-      socket.disconnect(); // Disconnect from the WebSocket server
-    };
-  }, []);
 
   return (
     <View style={styles.cont}>
